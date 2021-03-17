9b473707d4cd2a80aa9a7139182c0b59c9e9f33a
George Gu <minggu@amazon.com>
2021-03-17 14:54:37 +0800

    remove unused cli

--- source/cli/transcode.js
+++ source/cli/transcode.js
@@ -11,14 +11,10 @@ program
     .command('describe-task-dataset [parameters]', 'describes a dataset associated to a task')
     .command('describe-task-datasets [parameters]', 'describes the datasets associated with a task')
     .command('execute-task-crawler [parameters]', 'Starts a crawler for the specified task, regardless of what is scheduled. If the crawler is already running, the request is ignored.')
-    .command('get-task-crawler [parameters]', 'Retrieves crawler metadata for a specified task.')
-    .command('get-task-table-data [parameters]', 'Retrieves the external link to view table data in Amazon Athena.')
     .command('remove-task [parameters]', 'removes a task from the serverless video transcode')
     .command('remove-task-dataset [parameters]', 'removes a dataset from a task')
     .command('search [parameters]', 'search serverless video transcode')
     .command('update-task [parameters]', 'overwrites the details for a task')
-    .command('update-task-crawler [parameters]', 'Update the task crawler. If the task does not have one, a new crawler is created.')
-    .command('update-user-group-list [parameters]', 'Updates the list of groups that the user belongs to.')
     .command('upload-task-dataset [parameters]', 'uploads a new dataset file for a task');
 
 program.parse(process.argv);
